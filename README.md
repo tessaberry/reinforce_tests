ac8bfb9efd283ec0cd77e459413b88d6a881bd18
Tessa Berry <tessa.berry@here.com>
2020-02-22 05:50:26 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reinforcement Learning Algorithms
+
+## Credits
+These examples were originaly adapted from 
+`https://github.com/dragen1860/TensorFlow-2.x-Tutorials/blob/master/17-A2C/a2c.py`
---
+++ play_games/cartpole_with_A2C.py
@@ -0,0 +1,8 @@
+import gym
+import logging
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import tensorflow.keras.layers as kl
+import tensorflow.keras.losses as kls
+import tensorflow.keras.optimizers as ko
